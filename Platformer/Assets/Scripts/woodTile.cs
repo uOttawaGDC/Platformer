b1b2adb1dde96543aab444662ec5f94b5f2ce80a
Richard Hayes <richardhayes@Richards-MacBook-Pro.local>
2017-03-11 15:01:22 -0500

    working test of current assets. Open Platformer in unity to run game

---
+++ Platformer/Assets/Scripts/woodTile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class woodTile : MonoBehaviour {
+
+	private bool isDestroyable;
+
+
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			isDestroyable = true;
+		}
+		//isDestroyable = destroyObject.destroy;
+		if (isDestroyable) {
+
+			Destroy(gameObject);
+		}
+	}
+}
