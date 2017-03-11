56bd47dbc6009bb6419c1735db5ebff0a3eac6c1
Richard Hayes <richardhayes@Richards-MacBook-Pro.local>
2017-03-11 14:53:07 -0500

    working build

---
+++ Platformer/Assets/Ladder/ZoneOneController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneOneController : MonoBehaviour {
+
+	//
+	// required references
+	//
+	public GameObject ladder;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void OnTriggerEnter2D(Collider2D other){
+
+		if(other.tag == "Player" || true)
+			ladder.GetComponent<LadderController> ().hitZoneOne (other.gameObject);
+
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Platformer/Assets/Scripts/Vacuum.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vacuum : MonoBehaviour {
+	
+    public float gravityScale;
+    public GameObject character;
+    private Rigidbody2D rb;
+    private bool activated;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+		
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+		other.GetComponent<Bird> ().setGravityPower (gravityScale);
+
+    }
+	public void OnTriggerExit2D(Collider2D other){
+		other.GetComponent<Bird> ().setGravityPower (-2f);
+	}
+}
