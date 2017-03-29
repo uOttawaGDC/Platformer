45dbf18ed4cc8852662fb48c7687fde207d2fd1c
Richard Hayes <rhshadowgreen@gmail.com>
2017-03-29 11:17:07 -0400

    compiled prototype for web and added PlatformerDemo folder to project

---
+++ PlatformerBuild/Temp/StagingArea/Data/il2cppOutput/AssemblyU2DCSharp_Bounce1016091840.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+#include <stdint.h>
+
+
+#include "UnityEngine_UnityEngine_MonoBehaviour1158329972.h"
+#include "UnityEngine_UnityEngine_Vector22243707579.h"
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// Bounce
+struct  Bounce_t1016091840  : public MonoBehaviour_t1158329972
+{
+public:
+	// UnityEngine.Vector2 Bounce::fallingPosition
+	Vector2_t2243707579  ___fallingPosition_2;
+
+public:
+	inline static int32_t get_offset_of_fallingPosition_2() { return static_cast<int32_t>(offsetof(Bounce_t1016091840, ___fallingPosition_2)); }
+	inline Vector2_t2243707579  get_fallingPosition_2() const { return ___fallingPosition_2; }
+	inline Vector2_t2243707579 * get_address_of_fallingPosition_2() { return &___fallingPosition_2; }
+	inline void set_fallingPosition_2(Vector2_t2243707579  value)
+	{
+		___fallingPosition_2 = value;
+	}
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
---
+++ PlatformerBuild/Temp/StagingArea/Data/il2cppOutput/AssemblyU2DCSharp_ZoneTwoController2084728272MethodDeclarations.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+#include <stdint.h>
+#include <assert.h>
+#include <exception>
+
+// ZoneTwoController
+struct ZoneTwoController_t2084728272;
+// UnityEngine.Collider2D
+struct Collider2D_t646061738;
+
+#include "codegen/il2cpp-codegen.h"
+#include "UnityEngine_UnityEngine_Collider2D646061738.h"
+
+// System.Void ZoneTwoController::.ctor()
+extern "C"  void ZoneTwoController__ctor_m2488097657 (ZoneTwoController_t2084728272 * __this, const MethodInfo* method) IL2CPP_METHOD_ATTR;
+// System.Void ZoneTwoController::Start()
+extern "C"  void ZoneTwoController_Start_m276769473 (ZoneTwoController_t2084728272 * __this, const MethodInfo* method) IL2CPP_METHOD_ATTR;
+// System.Void ZoneTwoController::OnTriggerEnter2D(UnityEngine.Collider2D)
+extern "C"  void ZoneTwoController_OnTriggerEnter2D_m1996809113 (ZoneTwoController_t2084728272 * __this, Collider2D_t646061738 * ___other0, const MethodInfo* method) IL2CPP_METHOD_ATTR;
+// System.Void ZoneTwoController::Update()
+extern "C"  void ZoneTwoController_Update_m3283801374 (ZoneTwoController_t2084728272 * __this, const MethodInfo* method) IL2CPP_METHOD_ATTR;
